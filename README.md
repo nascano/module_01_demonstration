a504044c39c9868f6227ca9bc409b2de128f276d
Nathan Ascano <nascano@academic.rrc.ca>
2024-01-16 09:56:29 -0600

    Initial Commit: Created files with documentation.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Module 1 Demonstration
+
+## Description
+Hello world application
+Displays 'Hello World!' in multiple languages to the console.
+
+### Author
+Nathan Ascano
--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,8 @@ Description: Hello world display program
 Author: Nathan Ascano
 Date: 01/10/2024
 """
+
+print("Hello World!")
+print("Bonjour tout le monde!")
+#pronounced: tah-sih
+print ("tanisi!")
